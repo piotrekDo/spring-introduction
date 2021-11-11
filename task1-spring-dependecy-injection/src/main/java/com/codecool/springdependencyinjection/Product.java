35505767311f91030f003a7265981f5c10c04029
Peter <peter.zmilczak@gmail.com>
2021-11-11 19:47:16 +0100

    Some improvements

--- task1-spring-dependecy-injection/src/main/java/com/codecool/springdependencyinjection/Product.java
+++ task1-spring-dependecy-injection/src/main/java/com/codecool/springdependencyinjection/Product.java
@@ -1,16 +1,26 @@
 package com.codecool.springdependencyinjection;
 
 import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Product {
+
+    private static final AtomicInteger stupidIdGenerator = new AtomicInteger(1);
+
+    private final long id;
     private BigDecimal price;
 
     public Product(BigDecimal price) {
+        this.id = stupidIdGenerator.getAndIncrement();
         this.price = price;
     }
 
     public void applyDiscount(BigDecimal discount){
         this.price = price.subtract(discount);
+    }
+
+    public long getId() {
+        return id;
     }
 
     public BigDecimal getPrice() {
--- task1-spring-dependecy-injection/src/main/java/com/codecool/springdependencyinjection/ProductService.java
+++ task1-spring-dependecy-injection/src/main/java/com/codecool/springdependencyinjection/ProductService.java
@@ -3,6 +3,10 @@ package com.codecool.springdependencyinjection;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class ProductService {
