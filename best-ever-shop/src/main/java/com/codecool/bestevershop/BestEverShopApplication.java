b6eb9e245569a7369e1ae681f57aebae12434d24
Peter <peter.zmilczak@gmail.com>
2021-11-11 20:22:27 +0100

    best ever shop

---
+++ best-ever-shop/src/main/java/com/codecool/bestevershop/BestEverShopApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.bestevershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestEverShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestEverShopApplication.class, args);
+	}
+
+}
